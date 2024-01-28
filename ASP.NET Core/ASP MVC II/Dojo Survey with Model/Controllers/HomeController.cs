9cb14fb4cbe1985bc1f1996adba11883a8f87aa5
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2024-01-28 20:43:50 +0200

    Add files via upload

---
+++ ASP.NET Core/ASP MVC II/Dojo Survey with Model/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Dojo_Survey_with_Model.Models;
+
+namespace Dojo_Survey_with_Model.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    [HttpPost("survey")]
+        public IActionResult Submission(Survey yourSurvey)
+        {
+            // Handle your form submission here
+            return View(yourSurvey);
+        }
+}
---
+++ ASP.NET Core/ASP MVC II/Dojo Survey with Model/Models/Survey.cs
@@ -0,0 +1,9 @@
+public class Survey
+{
+    // your Survey fields go here
+    public string Name { get; set; }
+    public string Locations { get; set; }
+    public string Languages { get; set; }
+    public string Comments { get; set; }
+}
+
