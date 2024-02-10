d94fa68609df9b2e06c26a70c0ba1ee65c99d6b2
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2024-02-10 15:21:10 +0200

    Create Program.cs

---
+++ ORMs/Entity Framework/Assignment: Login and Registration/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Login_and_Registration.Models;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Adds database connection - must be before app.Build();
+builder.Services.AddDbContext<MyContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+builder.Services.AddSession();  // add this line before calling the builder.Build() method
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseSession();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
